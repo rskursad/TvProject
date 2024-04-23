d5c917ab347e322d81314aa48ef5ebf637ff5e17
Kürşad's PC <Krad's PC@DESKTOP-URFB7VL>
2024-04-23 18:08:47 +0300

    Add project files.

---
+++ ♥♥/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace __
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            PictureBox = new PictureBox();
+            guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            gucButton = new Guna.UI2.WinForms.Guna2Button();
+            Button1 = new Guna.UI2.WinForms.Guna2Button();
+            Button2 = new Guna.UI2.WinForms.Guna2Button();
+            Button3 = new Guna.UI2.WinForms.Guna2Button();
+            label1 = new Label();
+            kanalLabel = new Label();
+            guna2Elipse3 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            sinyalLabel = new Label();
+            guna2Elipse4 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            emptyScreen = new Label();
+            guna2Elipse5 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            infoLabel = new Label();
+            Button4 = new Guna.UI2.WinForms.Guna2Button();
+            Button5 = new Guna.UI2.WinForms.Guna2Button();
+            aktifKalite = new Label();
+            manzaranız = new Label();
+            ((System.ComponentModel.ISupportInitialize)PictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 50;
+            guna2Elipse1.TargetControl = this;
+            // 
+            // PictureBox
+            // 
+            PictureBox.BackColor = Color.Black;
+            PictureBox.Location = new Point(38, 37);
+            PictureBox.Name = "PictureBox";
+            PictureBox.Size = new Size(706, 438);
+            PictureBox.TabIndex = 0;
+            PictureBox.TabStop = false;
+            // 
+            // guna2Elipse2
+            // 
+            guna2Elipse2.BorderRadius = 50;
+            guna2Elipse2.TargetControl = PictureBox;
+            // 
+            // gucButton
+            // 
+            gucButton.BorderRadius = 25;
+            gucButton.CustomizableEdges = customizableEdges11;
+            gucButton.DisabledState.BorderColor = Color.DarkGray;
+            gucButton.DisabledState.CustomBorderColor = Color.DarkGray;
+            gucButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            gucButton.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            gucButton.FillColor = Color.Red;
+            gucButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            gucButton.ForeColor = Color.White;
+            gucButton.Location = new Point(694, 500);
+            gucButton.Name = "gucButton";
+            gucButton.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            gucButton.Size = new Size(50, 50);
+            gucButton.TabIndex = 1;
+            gucButton.Click += gucButton_Click;
+            // 
+            // Button1
+            // 
+            Button1.BorderRadius = 20;
+            Button1.CustomizableEdges = customizableEdges9;
+            Button1.DisabledState.BorderColor = Color.DarkGray;
+            Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button1.FillColor = Color.Black;
+            Button1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Button1.ForeColor = Color.White;
+            Button1.Location = new Point(38, 500);
+            Button1.Name = "Button1";
+            Button1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            Button1.Size = new Size(45, 45);
+            Button1.TabIndex = 2;
+            Button1.Click += Button1_Click;
+            // 
+            // Button2
+            // 
+            Button2.BorderRadius = 20;
+            Button2.CustomizableEdges = customizableEdges7;
+            Button2.DisabledState.BorderColor = Color.DarkGray;
+            Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button2.FillColor = Color.Black;
+            Button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Button2.ForeColor = Color.White;
+            Button2.Location = new Point(89, 500);
+            Button2.Name = "Button2";
+            Button2.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            Button2.Size = new Size(45, 45);
+            Button2.TabIndex = 3;
+            Button2.Click += Button2_Click;
+            // 
+            // Button3
+            // 
+            Button3.BorderRadius = 20;
+            Button3.CustomizableEdges = customizableEdges5;
+            Button3.DisabledState.BorderColor = Color.DarkGray;
+            Button3.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button3.FillColor = Color.Black;
+            Button3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Button3.ForeColor = Color.White;
+            Button3.Location = new Point(140, 500);
+            Button3.Name = "Button3";
+            Button3.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            Button3.Size = new Size(45, 45);
+            Button3.TabIndex = 4;
+            Button3.Click += Button3_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.OrangeRed;
+            label1.Font = new Font("Lucida Console", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(368, 509);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 25);
+            label1.TabIndex = 5;
+            label1.Text = "EK";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // kanalLabel
+            // 
+            kanalLabel.BackColor = Color.DarkCyan;
+            kanalLabel.Font = new Font("Lucida Console", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            kanalLabel.ForeColor = Color.White;
+            kanalLabel.Location = new Point(610, 52);
+            kanalLabel.Name = "kanalLabel";
+            kanalLabel.Size = new Size(117, 60);
+            kanalLabel.TabIndex = 6;
+            kanalLabel.Text = "MANZARA SHOW";
+            kanalLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // guna2Elipse3
+            // 
+            guna2Elipse3.BorderRadius = 50;
+            guna2Elipse3.TargetControl = kanalLabel;
+            // 
+            // sinyalLabel
+            // 
+            sinyalLabel.Font = new Font("Lucida Console", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            sinyalLabel.ForeColor = Color.White;
+            sinyalLabel.Location = new Point(285, 214);
+            sinyalLabel.Name = "sinyalLabel";
+            sinyalLabel.Size = new Size(228, 72);
+            sinyalLabel.TabIndex = 7;
+            sinyalLabel.Text = "Görüntü kalitesi ayarlarını yapmak için tuşları kullanın!";
+            sinyalLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // guna2Elipse4
+            // 
+            guna2Elipse4.BorderRadius = 50;
+            guna2Elipse4.TargetControl = sinyalLabel;
+            // 
+            // emptyScreen
+            // 
+            emptyScreen.BackColor = Color.Black;
+            emptyScreen.Location = new Point(38, 37);
+            emptyScreen.Name = "emptyScreen";
+            emptyScreen.Size = new Size(706, 438);
+            emptyScreen.TabIndex = 8;
+            emptyScreen.Text = "   ";
+            // 
+            // guna2Elipse5
+            // 
+            guna2Elipse5.BorderRadius = 50;
+            guna2Elipse5.TargetControl = emptyScreen;
+            // 
+            // infoLabel
+            // 
+            infoLabel.Font = new Font("Lucida Console", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            infoLabel.ForeColor = Color.Cyan;
+            infoLabel.Location = new Point(136, 9);
+            infoLabel.Name = "infoLabel";
+            infoLabel.Size = new Size(492, 25);
+            infoLabel.TabIndex = 9;
+            infoLabel.Text = "?- Televizyonu açmak için kırmızı tuşa bas";
+            infoLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Button4
+            // 
+            Button4.BorderRadius = 20;
+            Button4.CustomizableEdges = customizableEdges3;
+            Button4.DisabledState.BorderColor = Color.DarkGray;
+            Button4.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button4.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button4.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button4.FillColor = Color.Black;
+            Button4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Button4.ForeColor = Color.White;
+            Button4.Location = new Point(191, 500);
+            Button4.Name = "Button4";
+            Button4.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            Button4.Size = new Size(45, 45);
+            Button4.TabIndex = 10;
+            Button4.Click += Button4_Click;
+            // 
+            // Button5
+            // 
+            Button5.BorderRadius = 20;
+            Button5.CustomizableEdges = customizableEdges1;
+            Button5.DisabledState.BorderColor = Color.DarkGray;
+            Button5.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button5.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button5.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button5.FillColor = Color.Black;
+            Button5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Button5.ForeColor = Color.White;
+            Button5.Location = new Point(242, 500);
+            Button5.Name = "Button5";
+            Button5.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            Button5.Size = new Size(45, 45);
+            Button5.TabIndex = 11;
+            Button5.Click += Button5_Click;
+            // 
+            // aktifKalite
+            // 
+            aktifKalite.AutoSize = true;
+            aktifKalite.BackColor = Color.Black;
+            aktifKalite.ForeColor = Color.White;
+            aktifKalite.Location = new Point(38, 74);
+            aktifKalite.Name = "aktifKalite";
+            aktifKalite.Size = new Size(0, 20);
+            aktifKalite.TabIndex = 12;
+            // 
+            // manzaranız
+            // 
+            manzaranız.BackColor = Color.Black;
+            manzaranız.ForeColor = SystemColors.AppWorkspace;
+            manzaranız.Location = new Point(346, 303);
+            manzaranız.Name = "manzaranız";
+            manzaranız.Size = new Size(106, 25);
+            manzaranız.TabIndex = 13;
+            manzaranız.Text = "(Manzaranız)";
+            manzaranız.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(787, 587);
+            Controls.Add(manzaranız);
+            Controls.Add(aktifKalite);
+            Controls.Add(Button5);
+            Controls.Add(Button4);
+            Controls.Add(infoLabel);
+            Controls.Add(sinyalLabel);
+            Controls.Add(kanalLabel);
+            Controls.Add(label1);
+            Controls.Add(Button3);
+            Controls.Add(Button2);
+            Controls.Add(Button1);
+            Controls.Add(gucButton);
+            Controls.Add(PictureBox);
+            Controls.Add(emptyScreen);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)PictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private PictureBox PictureBox;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private Guna.UI2.WinForms.Guna2Button gucButton;
+        private Guna.UI2.WinForms.Guna2Button Button3;
+        private Guna.UI2.WinForms.Guna2Button Button2;
+        private Guna.UI2.WinForms.Guna2Button Button1;
+        private Label label1;
+        private Label kanalLabel;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse3;
+        private Label sinyalLabel;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse4;
+        private Label emptyScreen;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse5;
+        private Label infoLabel;
+        private Guna.UI2.WinForms.Guna2Button Button5;
+        private Guna.UI2.WinForms.Guna2Button Button4;
+        private Label aktifKalite;
+        private Label manzaranız;
+    }
+}
---
+++ ♥♥/Form1.cs
@@ -0,0 +1,136 @@
+﻿using static Guna.UI2.WinForms.Suite.Descriptions;
+using System.Media;
+
+namespace __
+{
+    public partial class Form1 : Form
+    {
+        private Image[] resimler1 = new Image[] { Properties.Resources._10, Properties.Resources._11, Properties.Resources._12, Properties.Resources._13, Properties.Resources._14
+        , Properties.Resources._15, Properties.Resources._16, Properties.Resources._17, Properties.Resources._18, Properties.Resources._19,Properties.Resources._20, Properties.Resources._21};
+        string resimYolu = "C:\\sizin\\resim\\yolunuz\\resim.jpg";
+        Image image = Image.FromFile(resimYolu);
+        private SoundPlayer player;
+        private int kapat;
+
+        public Form1()
+        {
+            InitializeComponent();
+            player = new SoundPlayer(Properties.Resources.mp31);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "TV";
+            kapat = 0;
+            PictureBox.Visible = false;
+            kanalLabel.Hide();
+            sinyalLabel.Hide();
+            sinyalLabel.Text = "Görüntü kalitesi ayarlarını yapmak için tuşları kullanın!";
+            manzaranız.Hide();
+        }
+        int count = -1;
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Pic(int deger)
+        {
+            PictureBox.Image = resimler1[deger];
+            PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void gucButton_Click(object sender, EventArgs e)
+        {
+            if (count == -1)
+            {
+                sinyalLabel.Show();
+                kanalLabel.Show();
+                infoLabel.Text = "?- Kırmızılarla devam et!";
+                Button1.FillColor = Color.Red;
+                gucButton.FillColor = Color.Black;
+                count++;
+            }
+            if (kapat==1)
+            {
+                player.Stop();
+                Close();
+            }
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (count == 1)
+            {
+                aktifKalite.Text = "Aktif Kalite: 480p";
+                Pic(8);
+                Button2.FillColor = Color.Black;
+                Button3.FillColor = Color.Red;
+                count++;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                aktifKalite.Text = "Aktif Kalite: 240p";
+                sinyalLabel.Hide();
+                Pic(0);
+                PictureBox.Visible = true;
+                infoLabel.Hide();
+                Button1.FillColor = Color.Black;
+                Button2.FillColor = Color.Red;
+                count++;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (count == 2)
+            {
+                aktifKalite.Text = "Aktif Kalite: 720p";
+                Pic(10);
+                Button3.FillColor = Color.Black;
+                Button4.FillColor = Color.Red;
+                count++;
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (count == 3)
+            {
+                aktifKalite.Text = "Aktif Kalite: 1080p";
+                Pic(11);
+                Button4.FillColor = Color.Black;
+                Button5.FillColor = Color.Red;
+                count++;
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (count == 4)
+            {
+                PictureBox.Image = image;
+                aktifKalite.Text = "Aktif Kalite: ∞";
+                manzaranız.Show();
+                player.PlayLooping();
+                kanalLabel.Text = "♥";
+                PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                kapat = 1;
+                Button5.FillColor = Color.Black;
+                gucButton.FillColor = Color.Red;
+                count++;
+            }
+            
+        }
+    }
+}
